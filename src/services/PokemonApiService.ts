23d0b417104824e7b5f39c9b9bcff6ab179af396
Tarcísio Carvalho <tarc.car@gmail.com>
2022-07-06 22:45:42 -0300

    feat: add to get radom pokemon id

--- src/services/PokemonApiService.ts
+++ src/services/PokemonApiService.ts
@@ -1,12 +1,17 @@
 import { GetRandomPokemonError } from '@/errors';
 import { Pokemon } from '@/types';
+import { getRandomNumber } from '@/utils';
 import { PokemonApiToPokemon } from './PokemonApiToPokemon';
 
-const URL_POKEMON_API = 'https://pokeapi.co/api/v2/pokemon/1';
+// TODO: colocar no arquivos env
+const URL_POKEMON_API = 'https://pokeapi.co/api/v2/pokemon';
+const MIN_POKEMON_ID_TO_CAPTURE = 1;
+const MAX_POKEMON_ID_TO_CAPTURE = 871;
 
 export class PokemonService {
   async getRandomPokemon (): Promise<Pokemon> {
-    const response = await fetch(URL_POKEMON_API)
+    const randomPokemonId = getRandomNumber(MIN_POKEMON_ID_TO_CAPTURE, MAX_POKEMON_ID_TO_CAPTURE)
+    const response = await fetch(`${URL_POKEMON_API}/${randomPokemonId}`)
     if (!response.ok) {
       throw new GetRandomPokemonError();
     }
