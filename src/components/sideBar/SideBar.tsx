bb953782f69691d2831fb32d1516486eed8f5a9b
Tarcísio Carvalho <tarc.car@gmail.com>
2022-07-08 00:00:30 -0300

    feat: add to open model on sidebar pokemon click

--- src/components/sideBar/SideBar.tsx
+++ src/components/sideBar/SideBar.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 
 import plusIcon from '@/assets/images/plus.png';
+import { selectPokemon } from '@/store/slices/PokemonSlice';
+import { useAppDispatch } from '@/store/storeHooks';
+import { Pokemon } from '@/types';
 
 import Button from '../button/Button';
 import CapturedPokemonSlots from './capturedPokemonSlots/CapturedPokemonSlots';
 import EmptySlots from './emptySlots/EmptySlots';
 
 const Sidebar: React.FC = () => {
+  const dispatch = useAppDispatch();
+
   const onAddCustomPokemonClickHandler = () => {
     console.log(`add custom`);
   };
 
+  const onSelectPokemonHandler = (pokemon: Pokemon) => {
+    dispatch(selectPokemon(pokemon));
+  };
+
   return (
     <div className="sidebar">
-      <CapturedPokemonSlots />
+      <CapturedPokemonSlots onSelect={onSelectPokemonHandler} />
 
       <EmptySlots />
 
--- src/components/sideBar/capturedPokemonSlots/CapturedPokemonSlots.tsx
+++ src/components/sideBar/capturedPokemonSlots/CapturedPokemonSlots.tsx
@@ -2,13 +2,25 @@ import React from 'react';
 
 import Avatar from '@/components/avatar/Avatar';
 import { useAppSelector } from '@/store/storeHooks';
+import { Pokemon } from '@/types';
 
-const CapturedPokemonSlots: React.FC = () => {
+type Props = {
+  onSelect: (pokemon: Pokemon) => void;
+};
+
+const CapturedPokemonSlots: React.FC<Props> = ({ onSelect }) => {
   const capturedPokemons = useAppSelector((state) => state.pokemon.captured);
+  const onCapturedPokemonClick = (pokemon: Pokemon) => {
+    onSelect(pokemon);
+  };
   return (
     <>
       {capturedPokemons.map((capturedPokemon) => (
-        <div className="sidebar__item" title={capturedPokemon.name}>
+        <div
+          className="sidebar__item"
+          title={capturedPokemon.name}
+          onClick={() => onCapturedPokemonClick(capturedPokemon)}
+        >
           <Avatar src={capturedPokemon.icon} alt={capturedPokemon.name} small />
         </div>
       ))}
